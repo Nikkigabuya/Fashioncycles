d2cc008fbe6212d15bafc64318831067452aa098
NikkiGabuya <35119245+Nikkigabuya@users.noreply.github.com>
2018-02-27 17:36:37 -0800

    Added boxes to first three sections

---
+++ main.js
@@ -0,0 +1,32 @@
+var shirt = document.getElementById("shirt");
+
+window.onscroll = function(){
+
+    var y = window.pageYOffset;
+  //  shirt.style.top = 200 + y  + "px";
+
+    if(y < 3000){
+      shirt.style.top= 4000 + y  + "px";
+
+    }else{
+
+    shirt.style.top = 200 + y  + "px";
+   }
+
+   if( y < 1000){
+     fabric.style.top= 4000 + y  + "px";
+
+   }else{
+
+   fabric.style.top = 100 + y  + "px";
+  }
+
+  if(y > 550){
+  document.getElementById("text_fabricleft").style.left = "50px";
+  } else {
+  document.getElementById("text_fabricleft").style.left = "-35px";
+  }
+
+
+
+};
