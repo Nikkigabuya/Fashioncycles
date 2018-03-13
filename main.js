783a1ab304518d08c6a6c8b32241fa7a2c3a9312
NikkiGabuya <35119245+Nikkigabuya@users.noreply.github.com>
2018-03-13 10:05:33 -0700

    Added pics and jscroll

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ window.onscroll = function(){
     var y = window.pageYOffset;
   //  shirt.style.top = 200 + y  + "px";
 
+
     if(y < 3000){
       shirt.style.top= 4000 + y  + "px";
 
@@ -13,25 +14,46 @@ window.onscroll = function(){
     shirt.style.top = 200 + y  + "px";
    }
 
-   if( y < 1000){
-     fabric.style.top= 4000 + y  + "px";
+  // if( y < 1000){
+//     fabric.style.top= 4000 + y  + "px";
 
-   }else{
+  // }else{
 
-   fabric.style.top = 100 + y  + "px";
-  }
+//   fabric.style.top = 100 + y  + "px";
+ //}
 
   if(y > 550){
-  document.getElementById("text_fabricleft").style.left = "50px";
+  document.getElementById("text_fabricleft").style.left = "100px";
   } else {
   document.getElementById("text_fabricleft").style.left = "-35px";
   }
 
   if(y > 800){
-  document.getElementById("text_fabricright").style.left = "800px";
+  document.getElementById("text_fabricright").style.left = "50px";
   } else {
-  document.getElementById("text_fabricright").style.left = "-35px";
+  document.getElementById("text_fabricright").style.right = "-35px";
+  }
+
+  if(y > 550){
+  document.getElementById("text_fabricleft2").style.left = "50px";
+  } else {
+  document.getElementById("text_fabricleft2").style.left = "-35px";
+  }
+
+  if(y > 800){
+  document.getElementById("text_fabricright2").style.left = "50px";
+  } else {
+  document.getElementById("text_fabricright2").style.right = "-35px";
   }
 
 
-};
+if (y > 900) {
+    Show();
+} else {
+    Hide();
+}
+
+
+
+
+;
