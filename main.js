43db38c9eadb8a90654dd1c8344cffe0585143f2
NikkiGabuya <35119245+Nikkigabuya@users.noreply.github.com>
2018-03-13 20:26:33 -0700

    Added pictures

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 var shirt = document.getElementById("shirt");
+var fabricroll = document.getElementById("fabricroll");
 
 window.onscroll = function(){
 
     var y = window.pageYOffset;
   //  shirt.style.top = 200 + y  + "px";
 
+ if(y >= 4050){
 
-    if(y < 3000){
-      shirt.style.top= 4000 + y  + "px";
+     shirt.style.top= 100 + y + "px";
 
-    }else{
+     }
+ else{
+   shirt.style.top = -900 + y  + "px";
+  }
 
-    shirt.style.top = 200 + y  + "px";
-   }
+
+
+
+  //  if(y < 3000){
+    //  shirt.style.top= 4000 + y  + "px";
+
+  //  }else{
+
+    //shirt.style.top = 200 + y  + "px";
+   //}
 
   // if( y < 1000){
 //     fabric.style.top= 4000 + y  + "px";
